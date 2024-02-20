26b691ff48dd1a4eb21fe4fc0f7493da104bfc1e
Tharindu wikasith dilshan <127284955+tharinduWik@users.noreply.github.com>
2024-02-21 00:28:30 +0530

    edit backend

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,4 @@
 const PORT = 5555;
 export default PORT;
+
+export const mongoDBURL = "mongodb+srv://tharinduwmaxx99:693321603v@cluster1.hfiw6hb.mongodb.net/";
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
-import express  from "express";
-import PORT from "./config";
-
-
+import express from "express";
+import {PORT,mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send({ message: "Hello, world!" });
+});
 app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
+});
+mongoose.connect(mongoDBURL)
+.then(() => {
+  console.log("DB connected");
+})
+.catch((err) => {
+  console.log(err);
 });
