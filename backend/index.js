41899771c4e504e973607c67b39fb74a6f4748ca
Tharindu wikasith dilshan <127284955+tharinduWik@users.noreply.github.com>
2024-02-22 09:54:31 +0530

    Create new book in Backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
-import {PORT,mongoDBURL} from "./config.js";
+import PORT from "./config.js";
+import {mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
+import {Book} from "./models/bookmodels.js";
 
 const app = express();
 
@@ -8,13 +10,32 @@ app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send({ message: "Hello, world!" });
 });
-app.listen(PORT, () => {
-  console.log(`app is running on port ${PORT}`);
-});
+
+app.post('/books',async(req,res)=>{
+  try{
+    if(!request.body.title || !request.body.author || !request.body.publishYear){
+      return res.status(400).send({message:"All fields are required"});
+    }
+    const book = new Book({
+      title:request.body.title,
+      author:request.body.author,
+      publishYear:request.body.publishYear
+    });
+
+    const Book = await Book.create(newBook);
+    return res.status(201).send(Book);
+  }catch(err){
+    console.log(err);
+  }
+
 mongoose.connect(mongoDBURL)
 .then(() => {
   console.log("DB connected");
+  app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
+  });
 })
 .catch((err) => {
   console.log(err);
+});
 });
--- backend/models/bookmodels.js
+++ backend/models/bookmodels.js
@@ -6,7 +6,16 @@ const bookShema = mongoose.Schema(
         title:{
             type:String,
             required: true,
-        }
-    }
+        },
+        Author:{
+            type:String,
+            required: true,
+        },
+        PubclishYear:{
+            type:Number,
+            required: true,
+        },
+    },
+    {timestamps:true}
 )
-export const Book = mongoose.model('cat',{name:String});
+export const Book = mongoose.model('cat',{bookShema});
